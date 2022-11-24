764f35e5c1bb8ed878e08ef1ee821b6ca23bfc8d
taosharma <tao@vidacycle.com>
2022-11-24 23:28:21 +0100

    Started to style topic tags.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,15 @@
-import HomeBannerImage from "../../components/HomeBannerImage";
-import Title from "../../components/Title";
-import DescriptionText from "../../components/DescriptionText";
-import Sidebar from "../../components/Sidebar";
-import ArticleSummaryCard from "../../components/ArticleSummaryCard";
+import HomeBannerImage from '../../components/HomeBannerImage'
+import Title from '../../components/Title'
+import DescriptionText from '../../components/DescriptionText'
+import Sidebar from '../../components/Sidebar'
+import ArticleSummaryCard from '../../components/ArticleSummaryCard'
+import Tag from '../../components/Tag'
 
-import styles from "./index.module.css";
-import SecondaryTitle from "../../components/SecondaryTitle";
-import FeaturedSummaryCard from "../../components/FeaturedSummaryCard";
+import styles from './index.module.css'
+import SecondaryTitle from '../../components/SecondaryTitle'
+import FeaturedSummaryCard from '../../components/FeaturedSummaryCard'
+
+const topics = ['News', 'Culture', 'Film & TV', 'Kids', 'Business', 'Travel']
 
 export default function Home() {
   return (
@@ -14,10 +17,15 @@ export default function Home() {
       <HomeBannerImage />
       <main className={styles.mainContainer}>
         <section className={styles.contentContainer}>
-          <Title titleText={"Welcome"} />
-          <SecondaryTitle titleText={"日本語で英語を学ぼう"} />
+          <Title titleText={'Welcome'} />
+          <SecondaryTitle titleText={'日本語で英語を学ぼう'} />
           <DescriptionText />
           <FeaturedSummaryCard />
+          <div className={styles.topicsContainer}>
+            {topics.map((topic) => (
+              <Tag size={'big'} text={topic} />
+            ))}
+          </div>
           <ArticleSummaryCard />
         </section>
         <section className={styles.sidebarContainer}>
@@ -25,5 +33,5 @@ export default function Home() {
         </section>
       </main>
     </>
-  );
+  )
 }
